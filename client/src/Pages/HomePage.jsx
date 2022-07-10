1572b6f1f8beb65faf3234940221dc89518a7cb1
Rudransh Dixit <dixitrudransh01@gmail.com>
2022-07-10 23:35:30 +0530

    added NavBar

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -4,9 +4,11 @@ import Hero from "../Componenets/Hero";
 import FeatureBox from "../Componenets/FeatureBox";
 import ForBox from "../Componenets/ForBox";
 import Footer from "../Componenets/Footer";
+import NavBar from "../Componenets/NavBar";
 const HomePage = () => {
   return (
     <div className="container bg-mainBg flex flex-wrap flex-col">
+      <NavBar />
       <CircularGradient />
       <Hero name="Customer" />
       <FeatureBox name="Customer" />
