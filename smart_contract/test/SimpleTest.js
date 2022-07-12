99fa22f5f1a3fa75c50c2d2bcf81dbdf26ea5f10
NetradeepakC <netradeepakc@gmail.com>
2022-07-12 17:38:15 +0530

    Truffle finally works(Migration was failing)

--- smart_contract/test/SimpleTest.js
+++ smart_contract/test/SimpleTest.js
@@ -1,11 +1,11 @@
 const License = artifacts.require("License");
 
-// contract("License",()=>{
-//     it('Mint license', async()=>{
-//         const recipient = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-//         const metadataURI = 'cid/test.png';
-//         const licensechain = LicenseChain.new();
-//         const License1 = await licensechain.safeMint(recipient, metadataURI, 10);
-//         console.log(License1);
-//     });
-// });
+contract("License",()=>{
+    it('Mint license', async()=>{
+        const recipient = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+        const metadataURI = 'cid/test.png';
+        const license = await License.new();
+        const License1 = await license.safeMint(recipient, metadataURI, 10);
+        console.log(License1);
+    });
+});
